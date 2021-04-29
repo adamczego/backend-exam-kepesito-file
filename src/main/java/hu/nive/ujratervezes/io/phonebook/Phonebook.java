ca808fd00919cb480e5b1f04579c2b7b6ef3a89e
adamczego <cz4d4m@gmail.com>
2021-04-29 12:03:54 +0200

    complete task

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,33 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        try(FileWriter fileWriter = new FileWriter(output)) {
+
+            contacts.forEach((name, phoneNumber) -> {
+                try {
+                    fileWriter.append(name + ": " + phoneNumber + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
